b1fe996d651617b6f5fa87abee296d97f43192e9
sphnikola <sphnikola@gmail.com>
2024-04-08 11:17:11 +0100

    Add navImages to be used for side nav icons

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -1,8 +1,68 @@
 import bars from "../../public/bars-solid.svg";
 import Image from "next/image";
 export default function Drawer() {
+  type NavImage = {
+    src: string;
+    href: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+
+  const navImages: NavImage[] = [
+    {
+      src: "/public/instagram-logo-24.png",
+      href: "#",
+      alt: "instagram icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/tiktok-logo-24.png",
+      href: "#",
+      alt: "tiktok icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/x-twitter.svg",
+      href: "#",
+      alt: "twitter icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/twitch.svg",
+      href: "#",
+      alt: "twitch icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/facebook.svg",
+      href: "#",
+      alt: "facebokk icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/discord-logo-24.png",
+      href: "#",
+      alt: "discord icon",
+      width: 24,
+      height: 24,
+    },
+    {
+      src: "/public/play-circle-regular-24.png",
+      href: "#",
+      alt: "play icon",
+      width: 24,
+      height: 24,
+    },
+  ];
+
   return (
-    <div>
+    <div className="flex flex-col gap-y-4">
       <div className="drawer drawer-end">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
