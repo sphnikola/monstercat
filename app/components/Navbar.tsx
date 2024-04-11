7137aa507deee9f76889a6c4a63ec66469a2255f
sphnikola <sphnikola@gmail.com>
2024-04-11 09:12:38 +0100

    remove commented out code

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,8 +7,6 @@ export default function Navbar() {
     <header className="  ">
       <div className=" ">
         <nav className=" flex flex-row justify-between items-center xl:items-start w-full fixed top-0 px-5 bg-[rgba(0,0,0,0.7)] xl:bg-[rgba(0,0,0,0)]">
-          {/* <h2>Hello Friend!</h2> */}
-          {/* <h3>mine</h3> */}
           <Image
             src={logo}
             alt="monstercat logo"
@@ -16,7 +14,6 @@ export default function Navbar() {
             height={50}
             className=" object-contain"
           />
-          {/* className="flex flex-row-reverse md:flex-col md:gap-y-6" */}
           <div className="hidden md:flex xl:hidden">
             <NavIcons />
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <>
       <div className="sectionBg"></div>
       <div className="">
-        {/* try to make navbar fixed */}
         <Navbar />
       </div>
       <div className="absolute top-[300px]">
