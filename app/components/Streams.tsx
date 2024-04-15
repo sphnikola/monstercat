8a327f6349b4918b644d50f1f3de1f9820149e97
sphnikola <sphnikola@gmail.com>
2024-04-15 16:14:32 +0100

    Add stream icons

--- app/components/Streams.tsx
+++ app/components/Streams.tsx
@@ -1,3 +1,9 @@
+import apple from "@/public/streamIcons/apple.svg";
+import bandcamp from "@/public/streamIcons/bandcamp.svg";
+import soundcloud from "@/public/streamIcons/soundcloud.svg";
+import spotify from "@/public/streamIcons/spotify.svg";
+import youtube from "@/public/streamIcons/youtube.svg";
+
 export default function Streams() {
   return (
     <section className="lg:ml-12">
