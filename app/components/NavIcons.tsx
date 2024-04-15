8fde8ebcd07644120496da69b415a85d45ec73b6
sphnikola <sphnikola@gmail.com>
2024-04-15 16:06:11 +0100

    change code for calling nav icons

--- app/components/NavIcons.tsx
+++ app/components/NavIcons.tsx
@@ -7,44 +7,52 @@ import tiktok from "../../public/tiktok-logo-24.png";
 import discord from "../../public/bxl-discord.svg";
 
 import Image from "next/image";
+interface Image {
+  name: string;
+  src: string;
+  alt: string;
+}
 
+const images: Image[] = [
+  { name: "x", src: x, alt: "navigation icons of twitter" },
+  { name: "twitch", src: twitch, alt: "navigation icons of twitch" },
+  {
+    name: "fb",
+    src: fb,
+    alt: "navigation icons of facebook",
+  },
+  {
+    name: "ig",
+    src: ig,
+    alt: "navigation icons of instagram",
+  },
+  {
+    name: "play",
+    src: play,
+    alt: "navigation icons of play",
+  },
+  {
+    name: "tiktok",
+    src: tiktok,
+    alt: "navigation icons of tiktok",
+  },
+  {
+    name: "discord",
+    src: discord,
+    alt: "navigation icons of discord",
+  },
+];
 export default function NavIcons() {
   return (
     <div className="xxl:mt-10">
-      <ul className="flex flex-row  gap-x-3 xxl:flex-col xxl:gap-x-5">
-        <li className=" text-[#874e4e]">
-          <Image
-            src={ig}
-            alt="instagram logo"
-            // width={24}
-            // height={24}
-          />
-        </li>
-        <li>
-          <Image src={twitch} alt="twitch logo" width={24} height={24} />
-        </li>
-        <li>
-          <Image src={x} alt="twitter logo" width={24} height={24} />
-        </li>
-        <li>
-          <Image src={discord} alt="discord logo" width={24} height={24} />
-        </li>
-        <li>
-          <Image
-            src={fb}
-            alt="facebook logo"
-            width={24}
-            height={24}
-            className=""
-          />
-        </li>
-        <li>
-          <Image src={tiktok} alt="instagram logo" width={24} height={24} />
-        </li>
-        <li>
-          <Image src={play} alt="instagram logo" width={24} height={24} />
-        </li>
+      <ul className="flex flex-row  gap-x-3 xxl:flex-col xxl:gap-y-5">
+        {images.map((img, index) => (
+          <li key={index}>
+            <Image src={img.src} alt={img.alt} width={24} height={24} />
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
+// export default NavIcons;
