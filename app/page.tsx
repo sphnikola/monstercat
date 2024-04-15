ecb25a9f74ecd82680a02eec683f17b7f4b789ed
sphnikola <sphnikola@gmail.com>
2024-04-15 13:39:01 +0100

    Add new section

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,20 @@ export default function Home() {
       </div>
 
       <div className=" absolute top-20 left-0 right-0 w-full px-2">
-        <div className="grid justify-center max-w-[500px] relative mx-auto lg:max-w-[712px]">
-          <div className="grid lg:grid-cols-[auto,auto]">
-            <Image src={albumCover} alt="cover image for album" className="" />
-            <p className=" lg:transform lg:-rotate-90 lg:absolute  -left-20 bottom-[228px]">
+        <div className="grid justify-center max-w-[500px] relative mx-auto lg:max-w-[712px] xl:grid-cols-2 xl:max-w-7xl  xl:items-end xl:gap-x-6 xl:justify-items-start ">
+          <div className="grid ">
+            <Image
+              src={albumCover}
+              alt="cover image for album"
+              className="xl:max-w-[400px] xl:ml-12 "
+            />
+            <p className=" lg:transform lg:-rotate-90 lg:absolute  lg:-left-20 lg:top-[465px] xl:top-[263px]">
               Instinct Released May 22, 2020{" "}
             </p>
           </div>
           {/* <span className="my-5 ">
           </span> */}
-          <div className="space-y-4">
+          <div className="space-y-4 ">
             <h3>level days</h3>
             <h4 className="">Conro</h4>
             <div className="space-x-1 ">
@@ -33,6 +37,10 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <section>
+          <h2>Stream it your way</h2>
+        </section>
       </div>
     </>
   );
